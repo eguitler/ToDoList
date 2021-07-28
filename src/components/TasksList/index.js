dda85061018e7ad5004b261b79fd674b160efb25
Ezequiel Guitler <eguitler@gmail.com>
2021-07-27 21:22:43 -0300

    removed console.log

--- src/components/TasksList/index.js
+++ src/components/TasksList/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import Task from "../Task";
 
 const TasksList = ({ title, tasks, onCheck }) => {
-    console.log(tasks);
     return (
         <div>
             <h3>{title}</h3>
             {[...tasks].reverse().map((task) => {
-                console.log(task);
                 return (
                     <Task
                         key={task.id}
