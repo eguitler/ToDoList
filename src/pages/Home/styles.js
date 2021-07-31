20c13013d9748e10ec184810d710e7a345b45803
Ezequiel Guitler <eguitler@gmail.com>
2021-07-31 12:30:13 -0300

    workaround to always show the scrollbar in order to avoid that the design shifts when show/hide done tasks

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
-    min-height: 100vh;
+    min-height: 100.1vh;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
